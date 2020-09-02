3abb28e6bde0cc816da6a1b11a9ad5ac29a76bcc
Paweł Kowalski <flukson@gmail.com>
2020-09-02 10:54:18 +0200

    Add train function

--- mnist.py
+++ mnist.py
@@ -51,3 +51,26 @@ if __name__ == '__main__':
   # Initialize the network and the optimizer:
   network = NeuralNetwork()
   optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
+
+  train_losses = []
+  train_counter = []
+  test_losses = []
+  test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+  def train(epoch):
+    network.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+      optimizer.zero_grad()
+      output = network(data)
+      loss = F.nll_loss(output, target)
+      loss.backward()
+      optimizer.step()
+      if batch_idx % log_interval == 0:
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+          epoch, batch_idx * len(data), len(train_loader.dataset),
+          100. * batch_idx / len(train_loader), loss.item()))
+        train_losses.append(loss.item())
+        train_counter.append(
+          (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+        torch.save(network.state_dict(), '/results/model.pth')
+        torch.save(optimizer.state_dict(), '/results/optimizer.pth')
