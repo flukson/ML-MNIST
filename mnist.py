da8528d6496b783dcb1de8ad50f3bd6a05242fee
Paweł Kowalski <flukson@gmail.com>
2020-09-02 10:56:11 +0200

    Add test function

--- mnist.py
+++ mnist.py
@@ -74,3 +74,19 @@ if __name__ == '__main__':
           (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
         torch.save(network.state_dict(), '/results/model.pth')
         torch.save(optimizer.state_dict(), '/results/optimizer.pth')
+
+  def test():
+    network.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+      for data, target in test_loader:
+        output = network(data)
+        test_loss += F.nll_loss(output, target, size_average=False).item()
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).sum()
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+    print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+      test_loss, correct, len(test_loader.dataset),
+      100. * correct / len(test_loader.dataset)))
