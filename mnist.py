89672a17968336a18134bb30a58fda91b70360ce
Paweł Kowalski <flukson@gmail.com>
2020-09-02 22:50:15 +0200

    Add argparse --mode option

--- mnist.py
+++ mnist.py
@@ -6,6 +6,7 @@
 # https://www.codementor.io/@dejanbatanjac/pytorch-the-missing-manual-on-loading-mnist-dataset-wjeh5top7
 # https://nextjournal.com/gkoehler/pytorch-mnist
 
+import argparse
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
@@ -31,29 +32,50 @@ t = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=(0), st
 
 if __name__ == '__main__':
 
-  # Download data:
-  mnist_trainset = datasets.MNIST(root=data_subdirectory, train=True, download=True, transform=t)
-  mnist_testset = datasets.MNIST(root=data_subdirectory, train=False, download=True, transform=t)
+  parser = argparse.ArgumentParser(description='Analyze coincidences file and plot results of the analysis',
+                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-  # Load data:
-  train_loader = DataLoader(mnist_trainset, batch_size=batch_size_train, drop_last=True, shuffle=True)
-  test_loader = DataLoader(mnist_testset, batch_size=batch_size_test, drop_last=True, shuffle=True)
+  parser.add_argument('-m', '--mode',
+                      dest='mode',
+                      type=str,
+                      default='calculate',
+                      help='mode of the script: calculate, plot')
 
-  # Initialize the network and the optimizer:
-  network = NeuralNetwork()
-  optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
+  args = parser.parse_args()
 
-  train_losses = []
-  train_counter = []
-  test_losses = []
-  test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+  if args.mode == "calculate":
 
-  # Run the training:
-  loss = test(network, test_loader) # this one is to evaluate model with randomly initialized parameters
-  test_losses.append(loss)
-  for epoch in range(1, n_epochs + 1):
-    loss, counter = train(network, epoch, train_loader, optimizer)
-    train_losses.append(loss)
-    train_counter.append(counter)
-    loss = test(network, test_loader)
+    # Download data:
+    mnist_trainset = datasets.MNIST(root=data_subdirectory, train=True, download=True, transform=t)
+    mnist_testset = datasets.MNIST(root=data_subdirectory, train=False, download=True, transform=t)
+
+    # Load data:
+    train_loader = DataLoader(mnist_trainset, batch_size=batch_size_train, drop_last=True, shuffle=True)
+    test_loader = DataLoader(mnist_testset, batch_size=batch_size_test, drop_last=True, shuffle=True)
+
+    # Initialize the network and the optimizer:
+    network = NeuralNetwork()
+    optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
+
+    train_losses = []
+    train_counter = []
+    test_losses = []
+    test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+    # Run the training:
+    loss = test(network, test_loader) # this one is to evaluate model with randomly initialized parameters
     test_losses.append(loss)
+    for epoch in range(1, n_epochs + 1):
+      loss, counter = train(network, epoch, train_loader, optimizer)
+      train_losses.append(loss)
+      train_counter.append(counter)
+      loss = test(network, test_loader)
+      test_losses.append(loss)
+
+  elif args.mode == "plot":
+
+    ""
+
+  else:
+
+    print("To check available modes run command: ./mnist.py -h")
