43863d08071c396d004a933599d3319f84f9c06c
Paweł Kowalski <flukson@gmail.com>
2020-09-02 22:31:38 +0200

    Reformat train and test functions

--- mnist.py
+++ mnist.py
@@ -128,28 +128,33 @@ if __name__ == '__main__':
         print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
           epoch, batch_idx * len(data), len(train_loader.dataset),
           100. * batch_idx / len(train_loader), loss.item()))
-        train_losses.append(loss.item())
-        train_counter.append((batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+        loss = loss.item()
+        counter = batch_idx*64 + (epoch-1)*len(train_loader.dataset)
         torch.save(network.state_dict(), './results/model.pth')
         torch.save(optimizer.state_dict(), './results/optimizer.pth')
+    return loss, counter
 
   def test():
     network.eval()
-    test_loss = 0
+    loss = 0
     correct = 0
     with torch.no_grad():
       for data, target in test_loader:
         output = network(data)
-        test_loss += F.nll_loss(output, target, size_average=False).item()
+        loss += F.nll_loss(output, target, size_average=False).item()
         pred = output.data.max(1, keepdim=True)[1]
         correct += pred.eq(target.data.view_as(pred)).sum()
-    test_loss /= len(test_loader.dataset)
-    test_losses.append(test_loss)
+    loss /= len(test_loader.dataset)
     print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-      test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+      loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+    return loss
 
   # Run the training:
-  test() # this one is to evaluate model with randomly initialized parameters
+  loss = test() # this one is to evaluate model with randomly initialized parameters
+  test_losses.append(loss)
   for epoch in range(1, n_epochs + 1):
-    train(epoch)
-    test()
+    loss, counter = train(epoch)
+    train_losses.append(loss)
+    train_counter.append(counter)
+    loss = test()
+    test_losses.append(loss)
